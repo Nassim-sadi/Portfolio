599b32850983cd88de42de295d5fab3e4d6a6ac3
nassim sadi <nacimbreeze@gmail.com>
2022-01-06 02:39:08 +0100

    added Notes

--- dist/script/script.js
+++ dist/script/script.js
@@ -39,11 +39,11 @@ $darkbtn = $("#darkmode");
 $darkbtnM = $("#darkmodemobile");
 $darkbtn.click(function () {
   htmlParent.classList.toggle("dark");
-  $darkbtn.toggleClass("bg-purple-400");
+  $darkbtn.toggleClass("bg-violet-400");
 });
 $darkbtnM.click(function () {
   htmlParent.classList.toggle("dark");
-  $darkbtnM.toggleClass("bg-purple-400");
+  $darkbtnM.toggleClass("bg-violet-400");
 });
 
 /* Open when someone clicks on the span element */
